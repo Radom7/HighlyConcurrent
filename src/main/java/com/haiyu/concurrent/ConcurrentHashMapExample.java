7afb4112b276713f2c67a5e5029578f028344250
liuxing <929593448@qq.com>
2018-10-23 09:32:44 +0800

    并发容器

---
+++ src/main/java/com/haiyu/concurrent/ConcurrentHashMapExample.java
@@ -0,0 +1,49 @@
+package com.haiyu.concurrent;
+
+import com.haiyu.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @Title: ArrayListExample
+ * @Description:
+ * @author: youqing
+ * @version: 1.0
+ * @date: 2018/10/22 16:40
+ */
+@Slf4j
+@ThreadSafe
+public class ConcurrentHashMapExample {
+    private static int threadTotal = 200;
+    private static int clientTotal = 5000;
+
+    private static Map<Integer,Integer> map = new ConcurrentHashMap <>();
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        for (int index = 0; index < clientTotal ; index++){
+            int count = index;
+            exec.execute(() -> {
+                try{
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                }catch (Exception e){
+                    log.error("exception",e);
+                }
+            });
+        }
+        exec.shutdown();
+        log.info("size:{}",map.size());
+    }
+
+    private static void update(int i){
+        map.put(i,i);
+    }
+}
---
+++ src/main/java/com/haiyu/concurrent/ConcurrentSkipListSetExample.java
@@ -0,0 +1,50 @@
+package com.haiyu.concurrent;
+
+
+import com.haiyu.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+import java.util.concurrent.*;
+
+/**
+ * @Title: ArrayListExample
+ * @Description:
+ * @author: youqing
+ * @version: 1.0
+ * @date: 2018/10/22 16:40
+ */
+@Slf4j
+@ThreadSafe
+public class ConcurrentSkipListSetExample {
+    private static int threadTotal = 200;
+    private static int clientTotal = 5000;
+
+    private static Set<Integer> set = new ConcurrentSkipListSet <>();
+
+    public static void main(String[] args) throws Exception{
+        ExecutorService exec = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int index = 0; index < clientTotal ; index++){
+            final int count = index;
+            exec.execute(() -> {
+                try{
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                }catch (Exception e){
+                    log.error("exception",e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        exec.shutdown();
+        log.info("size:{}",set.size());
+    }
+
+    private static void update(int i){
+        set.add(i);
+    }
+}
